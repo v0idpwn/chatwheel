5b642062136297ab3dc0e1a69a9c772d2f29f9a4
v0idpwn <v0idpwn@gmail.com>
2021-04-24 18:16:15 +0300

    Serve a list of audios

--- lib/chatwheel.ml
+++ lib/chatwheel.ml
@@ -1,2 +1,19 @@
-let _ = print_endline "Does it compile?"
-let _ = print_endline "Yes, it does"
+open Opium
+
+let sys_json _req =
+  let json : Yojson.Safe.t =
+    `Assoc [ "os-type", `String Sys.os_type; "ocaml-version", `String Sys.ocaml_version ]
+  in
+  Response.of_json json |> Lwt.return
+
+let show_audios _req =
+  let audios = 
+    Yojson.Safe.from_string (Utils.read_file "./priv/data/audio.json")
+  in
+    Lwt.return (Response.of_json audios)
+
+let _ = 
+  App.empty
+  |> App.get "/sys" sys_json 
+  |> App.get "/" show_audios
+  |> App.run_command
