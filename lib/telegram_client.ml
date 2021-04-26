d73e58761b014f05f63d0a59277b85a91632de4b
v0idpwn <v0idpwn@gmail.com>
2021-04-27 01:11:15 +0300

    Add content-type header

--- lib/telegram_client.ml
+++ lib/telegram_client.ml
@@ -7,8 +7,11 @@ let ( let* ) = Lwt.bind
 let url token = String.concat "" 
   ["https://api.telegram.org/bot/"; token; "/answerInlineQuery"]
 
+let header : Cohttp.Header.t =
+  Header.add (Header.init ()) "Content-Type" "application/json"
+
 let answer_inline_query (body : Cohttp_lwt.Body.t) : Unit.t Lwt.t =
   let token = Sys.getenv("TELEGRAM_BOT_TOKEN") in
   let uri = Uri.of_string (url token) in
-  let* (_resp, _body) = Client.post uri ~body:body in
+  let* (_resp, _body) = Client.post uri ~body:body ~headers:header in
   Lwt.return ()
