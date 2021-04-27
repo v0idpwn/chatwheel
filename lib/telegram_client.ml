9bb4d6c2a4b95effbaaaf9aae774ba02bdc22871
v0idpwn <v0idpwn@gmail.com>
2021-04-28 01:18:34 +0300

    Drain body

--- lib/telegram_client.ml
+++ lib/telegram_client.ml
@@ -13,5 +13,7 @@ let header : Cohttp.Header.t =
 let answer_inline_query (body : Cohttp_lwt.Body.t) : Unit.t Lwt.t =
   let token = Sys.getenv "TELEGRAM_BOT_TOKEN" in
   let uri = Uri.of_string (url token) in
-  let* _resp, _body = Client.post uri ~body ~headers:header in
+  let* response, _body = Client.post uri ~body ~headers:header in
+  let status = Response.status response in
+  if status <> `OK then Lwt.ignore_result (Cohttp_lwt.Body.drain_body body);
   Lwt.return ()
