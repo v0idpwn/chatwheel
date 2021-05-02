f20eb0514953bdc8cb866268113e2f5d1e0a2684
v0idpwn <v0idpwn@gmail.com>
2021-05-03 01:56:41 +0300

    Setup Bonsai

--- chatwheel_frontend/src/Chatwheel_frontend.ml
+++ chatwheel_frontend/src/Chatwheel_frontend.ml
@@ -1,4 +1,11 @@
-(* open Js_of_ocaml *)
-(* open Js *)
+open! Core_kernel
+open! Bonsai_web
 
-let _ = print_string "Hello, world!";
+(* let (_ : unit) = print_string "Hello, world!"; *)
+
+let component = Bonsai.const (Vdom.Node.text "hello world")
+
+let (_ : _ Start.Handle.t) =
+  Start.start_standalone ~initial_input:() ~bind_to_element_with_id:"app"
+  component
+;;
