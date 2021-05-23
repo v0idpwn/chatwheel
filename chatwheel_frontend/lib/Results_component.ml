01dba401814be6ce00a585fc44ff41a492361958
v0idpwn <v0idpwn@gmail.com>
2021-05-23 11:13:05 +0300

    Improve results layout

--- chatwheel_frontend/lib/Results_component.ml
+++ chatwheel_frontend/lib/Results_component.ml
@@ -2,12 +2,25 @@ open! Core_kernel
 open! Bonsai_web
 open! Frontend_common
 
+let div = Vdom.Node.div
+let classes = Vdom.Attr.classes
+let article = Vdom.Node.create "article"
+let figure = Vdom.Node.create "figure"
+let icon attrs = Vdom.Node.create "i" attrs []
+
 let render_result (result : Search_result.t) : Vdom.Node.t =
-  Vdom.Node.div [
-    Vdom.Attr.classes ["columns"]
+  article [
+    Vdom.Attr.classes ["media"]
   ] [
-    Vdom.Node.div [Vdom.Attr.classes ["column"]] [Vdom.Node.text "Picture"];
-    Vdom.Node.div [Vdom.Attr.classes ["column"]] [Vdom.Node.text result.name];
+    figure [ classes ["media-left"] ] [
+      Vdom.Node.button [classes ["button"; "is-primary"]] [
+        icon [classes ["fas"; "fa-play"]]
+      ]
+    ];
+    div [classes ["media-content"]] [
+      div [classes ["content"]] [Vdom.Node.h4 [] [Vdom.Node.text result.name]];
+    ];
+    div [classes ["media-right"]] [];
   ]
 
 let render (model : Search_bar.Model.t) =
