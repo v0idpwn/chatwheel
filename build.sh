fea171a5da095e1207711b86e3979c62f31932a2
v0idpwn <v0idpwn@gmail.com>
2021-06-05 20:45:30 +0300

    Base: Build script

---
+++ build.sh
@@ -0,0 +1,22 @@
+BUILD_ROOT=`pwd`
+rm -rf bin
+mkdir bin
+
+# building backend
+echo "Building backend..."
+cd $BUILD_ROOT/chatwheel_backend
+esy 
+echo "Finished building backend, moving..."
+cp _esy/default/build/default/lib/chatwheel_backend.exe $BUILD_ROOT/bin/server
+cp -r priv $BUILD_ROOT/bin/
+
+# bulding frontend
+echo "Building frontend..."
+cd $BUILD_ROOT/chatwheel_frontend
+esy
+npm install
+npm run webpack:production
+echo "Finished building frontend, moving..."
+cp -r build $BUILD_ROOT/bin/assets
+
+echo "Finished building"
